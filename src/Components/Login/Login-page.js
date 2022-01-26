a976769ba9ee593624b169a5b36b16f2155b27ce
andre-ramone <ramone_andre@hotmail.com>
2022-01-26 16:58:14 -0300

    feature: toast pagina login

--- src/Components/Login/Login-page.js
+++ src/Components/Login/Login-page.js
@@ -3,25 +3,47 @@ import LoginStyles from "../../main.scss";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../Services/auth";
 import api from "../../Services/api";
-import { Button } from "react-bootstrap";
+import { Button, Toast, ToastContainer } from "react-bootstrap";
 
 function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => setShow(!show);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     api.post("/tokens", { email }).then((res) => {
+      console.log(res);
       if (res.status === 200) {
         const result = res.data;
         login(result.token);
         navigate("/clientes");
       }
-    });
+    })
+    .catch((err) => {
+        setShow(true);
+    }) ;
   };
 
   return (
     <section id="entry-page">
+      <ToastContainer position="top-end">
+        <Toast show={show} onClose={toggleShow} bg="danger" autohide>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded me-2"
+                alt=""
+              />
+              <strong className="me-auto">Erro</strong>
+              <small></small>
+            </Toast.Header>
+            <Toast.Body className="text-white" >Email invalido!</Toast.Body>
+          </Toast>
+      </ToastContainer>
+
       <form onSubmit={handleSubmit}>
         <h3>Faça seu Login</h3>
         <fieldset>
